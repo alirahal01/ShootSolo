f57cb6de9cf12d0bc7521c8eca28a27656d551e1
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2025-01-07 23:46:48 +0200

    Setup privacy requriments , firebase and google

---
+++ ShootSolo/ShootSoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShootSoloApp.swift
+//  ShootSolo
+//
+//  Created by ali rahal on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShootSoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
